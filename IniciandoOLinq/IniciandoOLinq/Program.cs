8d8cb289478a8aa59806be96d23f9741aba31316
Luan Oliveira <luanvisouza037@gmail.com>
2025-09-09 18:13:22 -0300

    Criando a fonte de dados, e definindo a eepressao de consulta, onde todo numero par da fonte de dados tem que ser multiplicado por 10

---
+++ IniciandoOLinq/IniciandoOLinq/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace IniciandoOLinq;
+using System.Linq;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        //Especificar o data source, ou fonte de dados
+
+        int[] numbers = new[] { 2, 3, 4, 5 };
+        
+        //Definir a expressão de consulta
+
+        var result = numbers.Where(x => x % 2 == 0).Select(x => x * 10);
+    }
+}
